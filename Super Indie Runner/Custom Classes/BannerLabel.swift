8b9be63b5bb86948742b9eea99a55e5e59c854f9
KFischer (mbp_13) <klaus.fischer@eloware.com>
2020-11-29 11:42:40 +0100

    Added popups ?

--- Super Indie Runner/Custom Classes/BannerLabel.swift
+++ Super Indie Runner/Custom Classes/BannerLabel.swift
@@ -5,4 +5,22 @@
 import SpriteKit
 
 class BannerLabel: SKSpriteNode {
+
+    init(withTitle title: String){
+        let texture = SKTexture(imageNamed: GameConstants.StringConstants.bannerName)
+        super.init(texture: texture, color: UIColor.clear, size: texture.size())
+
+        let label = SKLabelNode(fontNamed: GameConstants.StringConstants.gameFontName)
+        label.fontSize = 200.0
+        label.verticalAlignmentMode = .center
+        label.text = title
+        label.scale(to: size, width: false, multiplier: 0.3)
+        label.zPosition = GameConstants.ZPositions.hudZ
+        addChild(label)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
 }
--- Super Indie Runner/Custom Classes/SpriteKitButton.swift
+++ Super Indie Runner/Custom Classes/SpriteKitButton.swift
@@ -6,4 +6,53 @@ import SpriteKit
 
 class SpriteKitButton: SKSpriteNode{
 
+    var defaultButton: SKSpriteNode
+    var action:(Int)->()
+    var index: Int
+
+    init(defaultButtonImage: String, action: @escaping (Int)->(), index: Int){
+        defaultButton = SKSpriteNode(fileNamed: defaultButtonImage)!
+        self.action = action
+        self.index = index
+
+        super.init(texture: nil, color: UIColor.clear, size: defaultButton.size)
+
+        isUserInteractionEnabled = true
+        addChild(defaultButton)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        defaultButton.alpha = 0.75
+
+    }
+
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = touches.first! as UITouch
+        let location : CGPoint = touch.location(in: self)
+
+        if defaultButton.contains(location){
+            defaultButton.alpha = 0.75
+        }
+        else {
+            defaultButton.alpha = 1.0
+        }
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let touch: UITouch = touches.first! as UITouch
+        let location : CGPoint = touch.location(in: self)
+
+        if defaultButton.contains(location){
+            action(index)
+        }
+        defaultButton.alpha = 1.0
+    }
+
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        defaultButton.alpha = 1.0
+    }
 }
